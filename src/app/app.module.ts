44e2d7d3e96301256b5f85dc298dd7304928a963
RatingRishu <rairishabh5001@gmail.com>
2024-03-28 14:20:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NewsEventComponent } from './news-event/news-event.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NewsEventComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/news-event/news-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-event',
+  templateUrl: './news-event.component.html',
+  styleUrls: ['./news-event.component.css']
+})
+export class NewsEventComponent {
+
+}
